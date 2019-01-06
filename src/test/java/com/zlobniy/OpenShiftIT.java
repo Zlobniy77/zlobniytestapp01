4a333d03c4ca8071ca530274513050880dbd648a
Zlobniy-mac <puffy-q@mail.ru>
2019-01-06 15:54:10 +0300

    turn off some extra tests

--- src/test/java/com/zlobniy/OpenShiftIT.java
+++ src/test/java/com/zlobniy/OpenShiftIT.java
@@ -1,31 +1,29 @@
 package com.zlobniy;
 
-import com.jayway.restassured.RestAssured;
-import com.zlobniy.greetings.GreetingProperties;
-import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
-import org.arquillian.cube.openshift.impl.enricher.RouteURL;
-import org.jboss.arquillian.junit.Arquillian;
-import org.junit.Before;
-import org.junit.runner.RunWith;
+//import com.jayway.restassured.RestAssured;
+//import com.zlobniy.greetings.GreetingProperties;
+//import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
+//import org.arquillian.cube.openshift.impl.enricher.RouteURL;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.junit.Before;
+//import org.junit.runner.RunWith;
 
-import java.net.URL;
-
-@RunWith(Arquillian.class)
-public class OpenShiftIT extends AbstractBoosterApplicationTest {
-
-    @AwaitRoute(path = "/health")
-    @RouteURL("${app.name}")
-    private URL baseURL;
-
-
-
-    @Before
-    public void setup() throws Exception {
-        RestAssured.baseURI = baseURL + "api/greeting";
-    }
-
-    protected GreetingProperties getProperties() {
-        return new GreetingProperties();
-    }
-
-}
+//@RunWith(Arquillian.class)
+//public class OpenShiftIT extends AbstractBoosterApplicationTest {
+//
+//    @AwaitRoute(path = "/health")
+//    @RouteURL("${app.name}")
+//    private URL baseURL;
+//
+//
+//
+//    @Before
+//    public void setup() throws Exception {
+//        RestAssured.baseURI = baseURL + "api/greeting";
+//    }
+//
+//    protected GreetingProperties getProperties() {
+//        return new GreetingProperties();
+//    }
+//
+//}
